edf498a4a5dc991a5333cbed0b073d84ed4db06b
Nithin Mallya <nithin@mallya.org>
2018-12-17 16:14:05 -0500

    kube changes

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,1 +1,3 @@
-kubectl apply -f ./kube/web-deployment.yml
+gcloud config set project nmallyatestproject
+gcloud container clusters get-credentials cd-cluster --zone us-central1-a --project nmallyatestproject
+kubectl apply -f ../kube/web-deployment.yml
