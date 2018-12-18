47df37b90bbedf65e0670f3d7e3740cb2deb7262
Nithin Mallya <nithin@mallya.org>
2018-12-17 23:55:59 -0500

    gpg testing

--- kube/generate_signature.sh
+++ kube/generate_signature.sh
@@ -3,19 +3,15 @@
 apt-get install rng-tools -y
 rngd -r /dev/urandom
 
-apt-get install gnupg2 -y
 
-echo "INSTALLED GNUPG2"
-echo "STOPPING AGENT GNUPG2"
-gpg-connect-agent /bye
 
 echo "STARTING GPG STUFF..."
 PROJECT_ID="nmallyatestproject"
 ATTESTOR_EMAIL="nithdevsecops@gmail.com"
 PGP_PUB_KEY="generated-key.pgp"
 
-gpg2 --quick-generate-key --yes ${ATTESTOR_EMAIL}
-gpg2 --armor --export "${ATTESTOR_EMAIL}" > ${PGP_PUB_KEY}
+gpg --quick-generate-key --yes ${ATTESTOR_EMAIL}
+gpg --armor --export "${ATTESTOR_EMAIL}" > ${PGP_PUB_KEY}
 #gcloud --project="${PROJECT_ID}" beta container binauthz attestors public-keys add --attestor="${ATTESTOR}" --public-key-file="${PGP_PUB_KEY}"
 #gcloud --project="${PROJECT_ID}" \
 #    beta container binauthz attestors list
