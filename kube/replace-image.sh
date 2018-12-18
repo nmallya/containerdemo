4f4fd93f5343a35307f1b866e3d37b0914c49dda
Nithin Mallya <nithin@mallya.org>
2018-12-17 20:00:58 -0500

    replacing image

--- kube/replace-image.sh
+++ kube/replace-image.sh
@@ -4,5 +4,7 @@ IMAGE_PATH="gcr.io/nmallyatestproject/containerdemo"
 IMAGE_DIGEST="$(gcloud container images list-tags --format='get(digest)' $IMAGE_PATH | head -1)"
 FINAL_IMAGE=$IMAGE_PATH@$IMAGE_DIGEST
 echo $FINAL_IMAGE
+pwd
+ls
 sed -i '' "s|MY_IMAGE|$FINAL_IMAGE|g" ./web-deployment.yml
 
