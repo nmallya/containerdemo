cdeb0337aca0314c118a2a7483d051489736cacb
Nithin Mallya <nithin@mallya.org>
2018-12-17 23:18:21 -0500

    gpg testing

--- kube/generate_signature.sh
+++ kube/generate_signature.sh
@@ -1,42 +1,14 @@
 #!/bin/bash
 
-apt-get install rng-tools -y
-rngd -r /dev/urandom
+sudo apt-get install rng-tools -y
+sudo rngd -r /dev/urandom
 
-
-GENERATED_PAYLOAD="generated_payload.json"
-GENERATED_SIGNATURE="generated_signature.pgp"
-
-ATTESTOR="manually-verified"
-ATTESTOR_EMAIL="$(gcloud config get-value core/account)"
+PROJECT_ID="nmallyatestproject"
+ATTESTOR_EMAIL="nithdevsecops@gmail.com"
 PGP_PUB_KEY="generated-key.pgp"
 
 gpg --quick-generate-key --yes ${ATTESTOR_EMAIL}
 gpg --armor --export "${ATTESTOR_EMAIL}" > ${PGP_PUB_KEY}
+gcloud --project="${PROJECT_ID}" beta container binauthz attestors public-keys add --attestor="${ATTESTOR}" --public-key-file="${PGP_PUB_KEY}"
 gcloud --project="${PROJECT_ID}" \
-    beta container binauthz attestors public-keys add \
-    --attestor="${ATTESTOR}" \
-    --public-key-file="${PGP_PUB_KEY}"
-
-
-PGP_FINGERPRINT="$(gpg --list-keys ${ATTESTOR_EMAIL} | head -2 | tail -1 | awk '{print $1}')"
-IMAGE_PATH="gcr.io/nmallyatestproject/containerdemo"
-IMAGE_DIGEST="$(gcloud container images list-tags --format='get(digest)' $IMAGE_PATH | head -1)"
-
-gcloud beta container binauthz create-signature-payload \
-    --artifact-url="${IMAGE_PATH}@${IMAGE_DIGEST}" > ${GENERATED_PAYLOAD}
-
-cat "${GENERATED_PAYLOAD}"
-
-gpg --local-user "${ATTESTOR_EMAIL}" \
-    --armor \
-    --output ${GENERATED_SIGNATURE} \
-    --sign ${GENERATED_PAYLOAD}
-
-gcloud beta container binauthz attestations create \
-    --artifact-url="${IMAGE_PATH}@${IMAGE_DIGEST}" \
-    --attestor="projects/${PROJECT_ID}/attestors/${ATTESTOR}" \
-    --signature-file=${GENERATED_SIGNATURE} \
-    --pgp-key-fingerprint="${PGP_FINGERPRINT}"
-
-#echo "projects/${PROJECT_ID}/attestors/${ATTESTOR}"
+    beta container binauthz attestors list
