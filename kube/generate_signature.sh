320e4edc370ce533b3f7cc7c272ac46252afc379
Nithin Mallya <nithin@mallya.org>
2018-12-18 12:33:23 -0500

    gpg testing

--- kube/generate_signature.sh
+++ kube/generate_signature.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-gpg2 --list-keys
+
+
+export GNUPGHOME="$(mktemp -d)"
+cat >foo <<EOF
+     %echo Generating a basic OpenPGP key
+     Key-Type: DSA
+     Key-Length: 1024
+     Subkey-Type: ELG-E
+     Subkey-Length: 1024
+     Name-Real: Joe Tester
+     Name-Comment: with stupid passphrase
+     Name-Email: joe@foo.bar
+     Expire-Date: 0
+     Passphrase: abc
+     # Do a commit here, so that we can later print "done" :-)
+     %commit
+     %echo done
+EOF
+gpg2 --batch --generate-key foo
+
+gpg2 --list-secret-keys
 #
 #echo "STARTING GPG STUFF..."
 #PROJECT_ID="nmallyatestproject"
