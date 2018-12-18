2c5a887aede5978956cd69da23184f10f3d4d8ae
Nithin Mallya <nithin@mallya.org>
2018-12-17 23:52:52 -0500

    gpg testing

--- kube/generate_signature.sh
+++ kube/generate_signature.sh
@@ -16,6 +16,6 @@ PGP_PUB_KEY="generated-key.pgp"
 
 gpg2 --quick-generate-key --yes ${ATTESTOR_EMAIL}
 gpg2 --armor --export "${ATTESTOR_EMAIL}" > ${PGP_PUB_KEY}
-gcloud --project="${PROJECT_ID}" beta container binauthz attestors public-keys add --attestor="${ATTESTOR}" --public-key-file="${PGP_PUB_KEY}"
-gcloud --project="${PROJECT_ID}" \
-    beta container binauthz attestors list
+#gcloud --project="${PROJECT_ID}" beta container binauthz attestors public-keys add --attestor="${ATTESTOR}" --public-key-file="${PGP_PUB_KEY}"
+#gcloud --project="${PROJECT_ID}" \
+#    beta container binauthz attestors list
