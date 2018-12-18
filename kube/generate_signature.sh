bc6a013e225f4576a3e40f310b338017ca884a7e
Nithin Mallya <nithin@mallya.org>
2018-12-18 11:54:39 -0500

    gpg testing

--- kube/generate_signature.sh
+++ kube/generate_signature.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-#apt-get install rng-tools -y
-#rngd -r /dev/urandom
-
-
 
 echo "STARTING GPG STUFF..."
 PROJECT_ID="nmallyatestproject"
-ATTESTOR_EMAIL="nithdevsecops@gmail.com"
+ATTESTOR_EMAIL="myemail@gmail.com"
 PGP_PUB_KEY="generated-key.pgp"
 
+export GPG_AGENT_INFO=${HOME}/.gnupg/S.gpg-agent:0:1
 gpg --quick-generate-key --yes ${ATTESTOR_EMAIL}
 gpg --armor --export "${ATTESTOR_EMAIL}" > ${PGP_PUB_KEY}
-#gcloud --project="${PROJECT_ID}" beta container binauthz attestors public-keys add --attestor="${ATTESTOR}" --public-key-file="${PGP_PUB_KEY}"
-#gcloud --project="${PROJECT_ID}" \
-#    beta container binauthz attestors list
