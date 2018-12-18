cc202fec7c07a8c9123927b96e8c4d88da4a4584
Nithin Mallya <nithin@mallya.org>
2018-12-18 12:07:33 -0500

    gpg testing

--- kube/generate_signature.sh
+++ kube/generate_signature.sh
@@ -6,10 +6,14 @@ PROJECT_ID="nmallyatestproject"
 ATTESTOR_EMAIL="myemail@gmail.com"
 PGP_PUB_KEY="generated-key.pgp"
 
-export GPG_AGENT_INFO=${HOME}/.gnupg/S.gpg-agent:0:1
+#export GPG_AGENT_INFO=${HOME}/.gnupg/S.gpg-agent:0:1
 echo "gpg agent is $GPG_AGENT_INFO"
 
-gpg --quick-generate-key --yes ${ATTESTOR_EMAIL}
+gpg --quick-generate-key --no-tty --batch --yes ${ATTESTOR_EMAIL}
+#gpg --quick-generate-key --yes ${ATTESTOR_EMAIL}
 gpg --armor --export "${ATTESTOR_EMAIL}" > ${PGP_PUB_KEY}
 
 
+
+
+gpg --quick-generate-key --no-tty --batch --yes ${ATTESTOR_EMAIL}
